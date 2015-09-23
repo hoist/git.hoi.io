496082451a80e6b593661a5479d5804a6e4cdcc8
Owen Evans <owen@bgeek.net>
2015-09-24 09:32:45 +1200

    use rimraf

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,7 @@ bunyanhub start
 #setup config
 echo "setting up app config"
 cp /config/production.json ./config/production.json
+cp /config/docker.json ./config/docker.json
 
 #start app
 echo "starting app"
