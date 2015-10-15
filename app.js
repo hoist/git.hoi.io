685861565671ddbe1450480c87117b33fe818d58
Owen Evans <owen@bgeek.net>
2015-10-16 10:57:06 +1300

    fix logs

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 'use strict';
+process.env.MUTE_LOGS = true;
 require("babel/register");
+var logger = require('@hoist/logger');
 process.title = 'git-listener';
 var server = require('./lib/server');
+var spawn = require('child_process').spawn;
 
-
-process.on('message', function (msg) {
-  console.log('got message', msg, msg === 'shutdown');
-  if (msg === 'shutdown') {
-    console.log('closing connection');
-    setTimeout(function () {
-      console.log('shutting down git server');
-      server.stop(function () {
-        process.exit(0);
-      });
-    }, 500);
-  }
+var loggerHub = spawn('bunyansub', ['-o', 'long', '--color'], {
+  stdio: 'inherit'
 });
+
+function gracefullShutdown(SIG) {
+  loggerHub.kill(SIG);
+  server.stop(function () {
+    logger.info('exiting process');
+    process.exit(0);
+  });
+}
 
 server.start(function (err) {
   if (err) {
     throw err;
   }
+  process.once('SIGUSR2', function () {
+    return gracefullShutdown('SIGUSR2');
+  });
+  process.once('SIGTERM', function () {
+    return gracefullShutdown('SIGTERM');
+  });
+  process.once('SIGINT', function () {
+    return gracefullShutdown('SIGINT');
+  });
   console.log('started');
 });
